c74d9ba7cc384a27c25baed0488de5eb62c79023
Josh <jrichard.smith98@gmail.com>
2024-03-28 11:58:06 +1000

    Update Testing with TransactionCollection tests

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MoneyManager.Core;
+
+TransactionCollection tc = new(0) {
+    new Transaction(10, "Michael", "Wage from michael"),
+    new Transaction(-35, "John", "John rent"),
+    new Transaction(500, "Tim", "Winnings")
+};
+
+foreach (Transaction t in tc)
+{
+    Console.WriteLine($"{t.Value}\t\t{t.Payee}\t\t{t.TransactionType}\t\t{tc.BalanceAt(t)}\t\t{t.Memo}");
+}
+Console.ReadKey();
