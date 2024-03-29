8061d558c53a0d0edb64c00a87761ce103dc0234
Josh <jrichard.smith98@gmail.com>
2024-03-29 14:00:43 +1000

    Added Account and Transfer tests

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,13 +1,26 @@
 ﻿using MoneyManager.Core;
 
-Account testAccount = new("TestAccount", 0, [
-    new Transaction(10, "Michael", "Wage from michael"),
-    new Transaction(-35, "John", "John rent"),
-    new Transaction(500, "Tim", "Winnings")
-]);
+Account testAccount1 = new("Spending", 0);
+testAccount1.AddTransaction(new Transaction(1500, "Michael", "Wage from michael"));
+testAccount1.AddTransactions(new Transaction(-200, "John", "John rent"), new Transaction(354.73M, "Tim", "Winnings"));
 
-foreach (Transaction t in testAccount.Transactions)
+Account testAccount2 = new("Savings", 1);
+testAccount1.TransferTo(1000, testAccount2, "Savings increment");
+testAccount1.TransferFrom(200, testAccount2, "Savings correction");
+
+Console.WriteLine($"{"Value",-20}   {"Payee",-20}   {"Type",-20}   {"Memo",-20}   {"Balance",-20}");
+Console.WriteLine();
+
+foreach (Transaction t in testAccount1.Transactions)
 {
-    Console.WriteLine($"{t.Value}\t\t{t.Payee}\t\t{t.TransactionType}\t\t{testAccount.BalanceAt(t)}\t\t{t.Memo}");
+    Console.WriteLine($"{t.Value,20} | {t.Payee,-20} | {t.TransactionType,-20} | {t.Memo,-20} | {testAccount1.BalanceAt(t),-20}");
 }
+Console.WriteLine();
+Console.WriteLine($"{"Value",-20}   {"Payee",-20}   {"Type",-20}   {"Memo",-20}   {"Balance",-20}");
+Console.WriteLine();
+foreach (Transaction t in testAccount2.Transactions)
+{
+    Console.WriteLine($"{t.Value,20} | {t.Payee,-20} | {t.TransactionType,-20} | {t.Memo,-20} | {testAccount1.BalanceAt(t),-20}");
+}
+
 Console.ReadKey();
