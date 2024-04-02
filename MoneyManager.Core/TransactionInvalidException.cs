eb91b960be3e3f92358eee50a77aa2ad2110f89e
Josh <jrichard.smith98@gmail.com>
2024-04-02 15:01:16 +1000

    Fixed accessibility of TransactionExceptions

--- MoneyManager.Core/TransactionInvalidException.cs
+++ MoneyManager.Core/TransactionInvalidException.cs
@@ -3,5 +3,5 @@
     /// <summary>
     /// Exception thrown when a <see cref="Transaction"/> that is invalid or of the incorrect sub-type is added to an <see cref="Account"/>.
     /// </summary>
-    internal class TransactionInvalidException(string message) : TransactionException(message) { }
+    public class TransactionInvalidException(string message) : TransactionException(message) { }
 }
