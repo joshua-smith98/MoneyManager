1f7d98285ad79b38d4822afaab6b1e339b9c1c91
Josh <jrichard.smith98@gmail.com>
2024-04-03 09:26:14 +1000

    Updated docstrings and comments

--- MoneyManager.Core/Sheet.cs
+++ MoneyManager.Core/Sheet.cs
@@ -1,7 +1,8 @@
-﻿using System.Security.Principal;
-
-namespace MoneyManager.Core
+﻿namespace MoneyManager.Core
 {
+    /// <summary>
+    /// Represents a financial sheet containing Accounts and Categories.
+    /// </summary>
     public class Sheet
     {
         public Account[] Accounts => accounts.ToArray();
@@ -12,6 +13,12 @@ namespace MoneyManager.Core
 
         public Sheet() { }
 
+        /// <summary>
+        /// Adds the given <see cref="Account"/> to this Sheet.
+        /// Throws a <see cref="SheetException"/> if it already exists within this Sheet.
+        /// </summary>
+        /// <param name="account"></param>
+        /// <exception cref="SheetException"></exception>
         public void NewAccount(Account account)
         {
             // Validity check: Accounts must not already contain account
@@ -19,14 +26,27 @@ namespace MoneyManager.Core
 
             accounts.Add(account);
         }
+
+        /// <summary>
+        /// Adds the given Accounts to this Sheet.
+        /// Throws a <see cref="SheetException"/> if any of the them already exist in this Sheet.
+        /// </summary>
+        /// <param name="accounts"></param>
+        /// <exception cref="SheetException"></exception>
         public void NewAccounts(params Account[] accounts)
         {
             // Validity check: Accounts must not already contain any of accounts
-            foreach (Account account in accounts)
+            foreach (Account account in accounts) // Use a foreach here rather than linq so we can access the account name
                 if (Accounts.Contains(account)) throw new SheetException($"Account \"{account.Name}\" already exists in sheet.");
 
             this.accounts.AddRange(accounts);
         }
+
+        /// <summary>
+        /// Removes the given <see cref="Account"/> from this Sheet.
+        /// </summary>
+        /// <param name="account"></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void DeleteAccount(Account account)
         {
             // Validity check: account must be within Accounts
@@ -34,6 +54,12 @@ namespace MoneyManager.Core
 
             accounts.Remove(account);
         }
+
+        /// <summary>
+        /// Removes the <see cref="Account"/> in this Sheet at the given index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void DeleteAccountAt(int index)
         {
             // Validity check: index must be within the range of accounts
@@ -42,6 +68,12 @@ namespace MoneyManager.Core
             accounts.RemoveAt(index);
         }
 
+        /// <summary>
+        /// Adds the given <see cref="Category"/> to this Sheet.
+        /// Throws a <see cref="SheetException"/> if the given Category already exists within this Sheet.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <exception cref="SheetException"></exception>
         public void NewCategory(Category category)
         {
             // Validity check: category must not already be in Categories
@@ -49,14 +81,27 @@ namespace MoneyManager.Core
 
             categories.Add(category);
         }
+
+        /// <summary>
+        /// Adds the given Categories to this Sheet.
+        /// Throws a <see cref="SheetException"/> if any of them already exist within this Sheet.
+        /// </summary>
+        /// <param name="categories"></param>
+        /// <exception cref="SheetException"></exception>
         public void NewCategories(params Category[] categories)
         {
             // Validity check: Categories must not already contain any of categories
-            foreach (Category category in categories)
-            if (Categories.Contains(category)) throw new SheetException($"Category \"{category.Name}\" already exists in sheet.");
+            foreach (Category category in categories) // Use a foreach here rather than linq so we can access the category name
+                if (Categories.Contains(category)) throw new SheetException($"Category \"{category.Name}\" already exists in sheet.");
 
             this.categories.AddRange(categories);
         }
+
+        /// <summary>
+        /// Removes the given <see cref="Category"/> from this Sheet, and detaches all <see cref="Transaction"/>s contained in that Category.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void DeleteCategory(Category category)
         {
             // Validity check: category must be contained within Categories
@@ -69,6 +114,11 @@ namespace MoneyManager.Core
             foreach (Transaction transaction in category.Transactions) transaction.Category = null;
         }
 
+        /// <summary>
+        /// Removes the <see cref="Category"/> at the given index from this Sheet, and detaches all <see cref="Transaction"/>s contained in that Category.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void DeleteCategoryAt(int index)
         {
             // Validity check: index must be within the bounds of categories
