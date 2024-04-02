f9251155fc9c68a1de88a2f7b4314cd432d97295
Josh <jrichard.smith98@gmail.com>
2024-04-02 19:15:08 +1000

    Refactored Budget with some new ideas from ChatGPT

--- MoneyManager.Core/Budget.cs
+++ MoneyManager.Core/Budget.cs
@@ -5,245 +5,58 @@
     /// </summary>
     public class Budget
     {
-        private Money? perDay;
-        private Money? perWeek;
-        private Money? perFortnight;
-        private Money? perMonth;
-        private Money? perQuarter;
-        private Money? per6Months;
-        private Money? perYear;
-
         /// <summary>
-        /// Gets or sets the amount of money per day.
+        /// Gets or sets the amount of money budgeted per day.
         /// </summary>
-        public Money PerDay
+        public Money PerDay 
         {
-            get
-            {
-                // Try to get from this period
-                if (perDay is not null) return perDay;
-
-                // Convert from other periods
-                // Here comes the madness...
-                if (perWeek is not null) return perWeek / 7;
-                if (perFortnight is not null) return perFortnight / 14;
-                if (perMonth is not null) return (perMonth * 12) / 365;
-                if (perQuarter is not null) return (perQuarter * 4) / 365;
-                if (per6Months is not null) return (per6Months * 2) / 365;
-                if (perYear is not null) return perYear / 365;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = value; // <-----
-
-                perWeek = null;
-                perFortnight = null;
-                perMonth = null;
-                perQuarter = null;
-                per6Months = null;
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Daily;
-            }
+            get => Get(Period.Daily);
+            set => Set(value, Period.Daily);
         }
 
         /// <summary>
-        /// Gets or sets the amount of money per week.
+        /// Gets or sets the amount of money budgeted per week.
         /// </summary>
         public Money PerWeek
         {
-            get
-            {
-                // Try to get from this period
-                if (perWeek is not null) return perWeek;
-
-                // Convert from other periods
-                // Here comes the madness...
-                if (perDay is not null) return perDay * 7;
-                if (perFortnight is not null) return perFortnight / 2;
-                if (perMonth is not null) return (perMonth * 12) / 52;
-                if (perQuarter is not null) return (perQuarter * 4) / 52;
-                if (per6Months is not null) return (per6Months * 2) / 52;
-                if (perYear is not null) return perYear / 52;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-
-                perWeek = value; // <-----
-
-                perFortnight = null;
-                perMonth = null;
-                perQuarter = null;
-                per6Months = null;
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Weekly;
-            }
+            get => Get(Period.Weekly);
+            set => Set(value, Period.Weekly);
         }
 
         /// <summary>
-        /// Gets or sets the amount of money per fortnight.
+        /// Gets or sets the amount of money budgeted per fortnight.
         /// </summary>
         public Money PerFortnight
         {
-            get
-            {
-                // Try to get from this period
-                if (perFortnight is not null) return perFortnight;
-
-                // Convert from other types
-                // Here comes the madness...
-                if (perDay is not null) return perDay * 14;
-                if (perWeek is not null) return perWeek * 2;
-                if (perMonth is not null) return (perMonth * 12) / 26;
-                if (perQuarter is not null) return (perQuarter * 4) / 26;
-                if (per6Months is not null) return (per6Months * 2) / 26;
-                if (perYear is not null) return perYear / 26;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-                perWeek = null;
-
-                perFortnight = value; // <-----
-
-                perMonth = null;
-                perQuarter = null;
-                per6Months = null;
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Fortnightly;
-            }
+            get => Get(Period.Fortnightly);
+            set => Set(value, Period.Fortnightly);
         }
 
         /// <summary>
-        /// Gets or sets the amount of money per month.
+        /// Gets or sets the amount of money budgeted per month.
         /// </summary>
         public Money PerMonth
         {
-            get
-            {
-                // Try to get from this period
-                if (perMonth is not null) return perMonth;
-
-                // Convert from other types
-                // Here comes the madness...
-                if (perDay is not null) return (perDay * 365) / 12;
-                if (perWeek is not null) return (perWeek * 52) / 12;
-                if (perFortnight is not null) return (perFortnight * 26) / 12;
-                if (perQuarter is not null) return perQuarter / 3;
-                if (per6Months is not null) return per6Months / 6;
-                if (perYear is not null) return perYear / 12;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-                perWeek = null;
-                perFortnight = null;
-
-                perMonth = value; // <-----
-
-                perQuarter = null;
-                per6Months = null;
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Monthly;
-            }
+            get => Get(Period.Monthly);
+            set => Set(value, Period.Monthly);
         }
 
         /// <summary>
-        /// Gets or sets the amount of money per quarter.
+        /// Gets or sets the amount of money budgeted per quarter.
         /// </summary>
         public Money PerQuarter
         {
-            get
-            {
-                // Try to get from this period
-                if (perQuarter is not null) return perQuarter;
-
-                // Convert from other types
-                // Here comes the madness...
-                if (perDay is not null) return (perDay * 365) / 4;
-                if (perWeek is not null) return (perWeek * 52) / 4;
-                if (perFortnight is not null) return (perFortnight * 26) / 4;
-                if (perMonth is not null) return perMonth * 3;
-                if (per6Months is not null) return per6Months / 2;
-                if (perYear is not null) return perYear / 4;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-                perWeek = null;
-                perFortnight = null;
-                perMonth = null;
-
-                perQuarter = value; // <-----
-
-                per6Months = null;
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Quarterly;
-            }
+            get => Get(Period.Quarterly);
+            set => Set(value, Period.Quarterly);
         }
 
         /// <summary>
-        /// Gets or sets the amount of money every 6 months.
+        /// Gets or sets the amount of money budgeted per 6 months.
         /// </summary>
         public Money Per6Months
         {
-            get
-            {
-                // Try to get from this period
-                if (per6Months is not null) return per6Months;
-
-                // Convert from other types
-                // Here comes the madness...
-                if (perDay is not null) return (perDay * 365) / 2;
-                if (perWeek is not null) return (perWeek * 52) / 2;
-                if (perFortnight is not null) return (perFortnight * 26) / 2;
-                if (perMonth is not null) return perMonth * 6;
-                if (perQuarter is not null) return perQuarter * 2;
-                if (perYear is not null) return perYear / 2;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-                perWeek = null;
-                perFortnight = null;
-                perMonth = null;
-                perQuarter = null;
-
-                per6Months = value; // <-----
-
-                perYear = null;
-
-                // Set the base period
-                CurrentPeriod = Period.Biannually;
-            }
+            get => Get(Period.Biannually);
+            set => Set(value, Period.Biannually);
         }
 
         /// <summary>
@@ -251,105 +64,68 @@
         /// </summary>
         public Money PerYear
         {
-            get
-            {
-                // Try to get from this period
-                if (perYear is not null) return perYear;
-
-                // Convert from other types
-                // Here comes the madness...
-                if (perDay is not null) return perDay * 365;
-                if (perWeek is not null) return perWeek * 52;
-                if (perFortnight is not null) return perFortnight * 26;
-                if (perMonth is not null) return perMonth * 6;
-                if (perQuarter is not null) return perQuarter * 4;
-                if (per6Months is not null) return per6Months * 2;
-
-                throw new BudgetException("All Budget values are empty!");
-            }
-            set
-            {
-                // Set this period to value, and all others to null
-                perDay = null;
-                perWeek = null;
-                perFortnight = null;
-                perMonth = null;
-                perQuarter = null;
-                per6Months = null;
-
-                perYear = value; // <-----
-
-                // Set the base period
-                CurrentPeriod = Period.Annually;
-            }
+            get => Get(Period.Annually);
+            set => Set(value, Period.Annually);
         }
 
+        private Money perDay;
+
         /// <summary>
-        /// The base period this budget is currently using.
+        /// Gets the current base period for this budget.
         /// </summary>
         public Period CurrentPeriod { get; private set; }
 
         public Budget(Money value, Period period)
         {
-            // Set period field to value based on period given
-            switch (period)
-            {
-                case Period.Daily:
-                    perDay = value;
-                    break;
-                case Period.Weekly:
-                    perWeek = value;
-                    break;
-                case Period.Fortnightly:
-                    perFortnight = value;
-                    break;
-                case Period.Monthly:
-                    perMonth = value;
-                    break;
-                case Period.Quarterly:
-                    perQuarter = value;
-                    break;
-                case Period.Biannually:
-                    per6Months = value;
-                    break;
-                case Period.Annually:
-                    perYear = value;
-                    break;
-                default:
-                    throw new BudgetException("Budget must be defined with a valid period!");
-            }
-
-            // Set the base period
-            CurrentPeriod = period;
+            Set(value, period); // Set() sets the value of perDay, so just ignore the IDE's complaints here
         }
 
         /// <summary>
-        /// Gets a <see cref="Money"/> value from this <see cref="Budget"/> for the given <see cref="Period"/>.
+        /// Gets the amount of money budgeted for the given period.
         /// </summary>
         /// <param name="period"></param>
         /// <returns></returns>
-        /// <exception cref="BudgetException"></exception>
-        public Money Get(Period period)
+        public Money Get(Period period) => FromDaily(perDay, period);
+        
+        /// <summary>
+        /// Sets the amount of money budgeted for the given period.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="period"></param>
+        public void Set(Money value, Period period)
         {
-            switch (period)
+            perDay = ToDaily(value, period);
+            CurrentPeriod = period;
+        }
+
+        private Money ToDaily(Money value, Period fromPeriod)
+        {
+            return fromPeriod switch
             {
-                case Period.Daily:
-                    return PerDay;
-                case Period.Weekly:
-                    return PerWeek;
-                case Period.Fortnightly:
-                    return PerFortnight;
-                case Period.Monthly:
-                    return PerMonth;
-                case Period.Quarterly:
-                    return PerQuarter;
-                case Period.Biannually:
-                    return Per6Months;
-                case Period.Annually:
-                    return PerYear;
-                default:
-                    throw new BudgetException("Budget must be accessed with a valid period!");
-            }
+                Period.Daily => value,
+                Period.Weekly => value / 7,
+                Period.Fortnightly => value / 14,
+                Period.Monthly => value * 12 / 365,
+                Period.Quarterly => value * 4 / 365,
+                Period.Biannually => value * 2 / 365,
+                Period.Annually => value / 365,
+                _ => throw new ArgumentOutOfRangeException(nameof(fromPeriod), "Period must not be \"Period.Null\".")
+            };
+        }
+
+        private Money FromDaily(Money dailyValue, Period toPeriod)
+        {
+            return toPeriod switch
+            {
+                Period.Daily => dailyValue,
+                Period.Weekly => dailyValue * 7,
+                Period.Fortnightly => dailyValue * 14,
+                Period.Monthly => dailyValue * 365 / 12,
+                Period.Quarterly => dailyValue * 365 / 4,
+                Period.Biannually => dailyValue * 365 / 2,
+                Period.Annually => dailyValue * 365,
+                _ => throw new ArgumentOutOfRangeException(nameof(toPeriod), "Period must not be \"Period.Null\".")
+            };
         }
     }
 }
