716bdd98c861a6c7421521e74aa93a7910615131
Josh <jrichard.smith98@gmail.com>
2024-04-08 16:01:23 +1000

    Added new GetNumberInOrNull() extension method to Period to help with generating stepped reports

--- MoneyManager.Core/Period.cs
+++ MoneyManager.Core/Period.cs
@@ -29,5 +29,44 @@
                 Period.Annually => startDate.AddYears(1).AddDays(-1),
                 _ => throw new ArgumentOutOfRangeException(nameof(period), "Period must not be null!")
             };
+
+        public static int? GetNumberInOrNull(this Period smallPeriod, Period bigPeriod)
+            => bigPeriod switch
+            {
+                Period.Daily => null, // Daily cannot be divided into any other period (except itself, which for the purpose of this app doesn't count)
+                Period.Weekly => smallPeriod is Period.Daily ? 7 : null,
+                Period.Fortnightly => smallPeriod switch
+                {
+                    Period.Daily => 14,
+                    Period.Weekly => 2,
+                    _ => null
+                },
+                Period.Monthly => null, // Months cannot be evenly divided into any other period
+                Period.Quarterly => smallPeriod switch
+                {
+                    Period.Weekly => 13,
+                    Period.Monthly => 3,
+                    _ => null
+                },
+                Period.Biannually => smallPeriod switch
+                {
+                    Period.Weekly => 26,
+                    Period.Fortnightly => 13,
+                    Period.Monthly => 6,
+                    Period.Quarterly => 2,
+                    _ => null
+                },
+                Period.Annually => smallPeriod switch
+                {
+                    Period.Daily => 365,
+                    Period.Weekly => 52,
+                    Period.Fortnightly => 26,
+                    Period.Monthly => 12,
+                    Period.Quarterly => 4,
+                    Period.Biannually => 2,
+                    _ => null
+                },
+                _ => null
+            };
     }
 }
