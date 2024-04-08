d4940781c5c6256a8a6ed9861f95ae877f393230
Josh <jrichard.smith98@gmail.com>
2024-04-08 15:11:44 +1000

    Created and implemented ReportChunk class

---
+++ MoneyManager.Core/ReportChunk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MoneyManager.Core
+{
+    public class ReportChunk
+    {
+        public ReportChunkCategory[] ReportChunkCategories { get; }
+
+        public BalanceInfo BalanceInfo { get; }
+        public BalanceInfo BalanceInfoToDate { get; }
+
+        public Period Period { get; }
+        public DateOnly StartDate { get; }
+        public DateOnly EndDate => Period.GetEndDate(StartDate);
+
+        internal ReportChunk(DateOnly startDate, Period period, BalanceInfo balanceInfo, BalanceInfo balanceInfoToDate, ReportChunkCategory[] reportChunkCategories)
+        {
+            StartDate = startDate;
+            Period = period;
+            BalanceInfo = balanceInfo;
+            BalanceInfoToDate = balanceInfoToDate;
+            ReportChunkCategories = reportChunkCategories;
+        }
+    }
+}
