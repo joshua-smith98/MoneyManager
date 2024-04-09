ae27f2f088f52849d5e7862784e972a593a30889
Josh <jrichard.smith98@gmail.com>
2024-04-10 08:24:29 +1000

    Allowed the 'from' and 'to' arguments in Balanceable.BalanceInfoBetweenIndicies() to be equal, for the possibility of getting a range with only a single transaction

--- MoneyManager.Core/Balanceable.cs
+++ MoneyManager.Core/Balanceable.cs
@@ -69,7 +69,7 @@
         public virtual BalanceInfo BalanceInfoBetweenIndices(int from, int to)
         {   
             // Validity check, from must be less then to
-            if (from >= to) throw new ArgumentOutOfRangeException(nameof(from), "Argument 'from' must be less than argument 'to'.");
+            if (from > to) throw new ArgumentOutOfRangeException(nameof(from), "Argument 'from' must be less than or equal to argument 'to'.");
 
             // Validity check: from and to must be within the bounds of Transactions[]
             if (from < 0 || to >= Transactions.Length) throw new IndexOutOfRangeException(); // Since 'from' must be less than 'to', we only need to check 'to' here
