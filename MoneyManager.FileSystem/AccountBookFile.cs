3ef128a01b4ddc81c0dafdb6ed2641dd370aef0c
Josh <jrichard.smith98@gmail.com>
2024-04-21 15:28:01 +1000

    Created AccountBookFile records and data structure

--- MoneyManager.FileSystem/AccountBookFile.cs
+++ MoneyManager.FileSystem/AccountBookFile.cs
@@ -12,8 +12,67 @@ namespace MoneyManager.FileSystem
 
         public string? Path { get; private set; }
 
-        private AccountBookFile(string? path)
-            => Path = path;
+        internal int NumCategories;
+        internal CategoryTableRow[] CategoryTable;
+        internal int NumAccounts;
+        internal AccountTableRow[] AccountTable;
+        internal int NumTransfers;
+        internal TransferTableRow[] TransferTable;
+
+        internal record CategoryTableRow(string Name)
+        {
+            public BudgetDat? IncomeBudget { get; init; }
+            public BudgetDat? ExpensesBudget { get; init; }
+        }
+
+        internal record BudgetDat(decimal PerDay, int Period);
+
+        internal record AccountTableRow(
+            string Name,
+            int NumTransactions,
+            TransactionTableRow[] TransactionTable
+            );
+
+        internal record TransactionTableRow(
+            string TransactionNumber,
+            decimal Value,
+            int DayNumber,
+            string Payee,
+            string Memo,
+            string CategoryName,
+            bool IsCleared
+            );
+
+        internal record TransferTableRow(
+            string FromAccName,
+            string ToAccName,
+            string TransactionNumber,
+            decimal Value,
+            int DayNumber,
+            string Payee,
+            string Memo,
+            string CategoryName,
+            bool IsCleared
+            );
+
+        private AccountBookFile(
+            int numCategories,
+            CategoryTableRow[] categoryTable,
+            int numAccounts,
+            AccountTableRow[] accountTable,
+            int numTransfers,
+            TransferTableRow[] transferTable,
+            string? path = null
+            )
+        {
+            NumCategories = numCategories;
+            CategoryTable = categoryTable;
+            NumAccounts = numAccounts;
+            AccountTable = accountTable;
+            NumTransfers = numTransfers;
+            TransferTable = transferTable;
+            Path = path;
+        }
 
         public static AccountBookFile LoadFrom(string path)
         {
