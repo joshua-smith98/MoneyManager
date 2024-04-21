196f0753a081611fb26fc803dc97f602b5788e9a
Josh <jrichard.smith98@gmail.com>
2024-04-21 15:08:37 +1000

    Created new AccountBookFile class

---
+++ MoneyManager.FileSystem/AccountBookFile.cs
@@ -0,0 +1,44 @@
+﻿using MoneyManager.Core;
+
+namespace MoneyManager.FileSystem
+{
+    public class AccountBookFile : IFile<AccountBook, AccountBookFile>
+    {
+        public static char[] Header => "MOMAACBK".ToArray();
+
+        public static string Extension => ".accbk";
+
+        public static int Version => 1;
+
+        public string? Path { get; private set; }
+
+        private AccountBookFile(string? path)
+            => Path = path;
+
+        public static AccountBookFile LoadFrom(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AccountBookFile Deconstruct(AccountBook accountBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFrom(AccountBook accountBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTo(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountBook Construct()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
