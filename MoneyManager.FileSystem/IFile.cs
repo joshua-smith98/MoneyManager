2d8777af12b60938241f6e878876f6d022f6b70c
Josh <jrichard.smith98@gmail.com>
2024-04-21 15:03:32 +1000

    Created new IFile interface

---
+++ MoneyManager.FileSystem/IFile.cs
@@ -0,0 +1,18 @@
+﻿namespace MoneyManager.FileSystem
+{
+    public interface IFile<T, This>
+    {
+        public static abstract char[] Header { get; }
+        public static abstract string Extension { get; }
+        public static abstract int Version { get; }
+
+        public string? Path { get; }
+
+        public static abstract This LoadFrom(string path);
+        public static abstract This Deconstruct(T t);
+
+        public void UpdateFrom(T t);
+        public void SaveTo(string path);
+        public T Construct();
+    }
+}
