f03eb3b2ef24229403337ce59bb28e43e2cc0ff0
joshua-smith98 <jrichard.smith98@gmail.com>
2024-05-04 18:55:40 +1000

    Created REPL Command class

---
+++ MoneyManager.REPL/Command.cs
@@ -0,0 +1,34 @@
+﻿namespace MoneyManager.REPL
+{
+    internal abstract class Command
+    {
+        public abstract string Str { get; }
+
+        public abstract string About { get; }
+        public string CommandPath
+        {
+            get
+            {
+                // Build CommandPath
+            }
+        }
+
+        public abstract Command[] SubCommands { get; }
+        public abstract Argument[] Arguments { get; }
+
+        public abstract Action<ArgumentValueCollection>? Action { get; }
+
+        private string _pathToThisCommand;
+
+        public Command(string pathToThisCommand)
+        {
+            // Validate constraints
+
+            
+            _pathToThisCommand = pathToThisCommand;
+        }
+
+        public bool MatchStr(string commandSubStr)
+            => commandSubStr.Split().First().ToLower() == Str.ToLower();
+    }
+}
